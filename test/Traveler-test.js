fdf34be89235d662949ecf8d6a829801b8ac09f4
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-02 15:45:17 -0600

    Add test data to test data file

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,12 +1,15 @@
 import chai from 'chai';
 const expect = chai.expect;
+import Traveler from './Traveler';
+import testData from './test-data';
 
 describe('See if the tests are running', function() {
 
   beforeEach(() => {
-  }
+    console.log(testData)
+  })
 
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
+  // it('should return true', function() {
+  //   expect(true).to.equal(true);
+  // });
 });
